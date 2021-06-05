d013d1774f61f3241a64f9d3eb7e0caac0a14ace
Ozodchik <dilovardehkanov730@gmail.com>
2021-06-05 12:26:36 +0300

    success

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,4 @@ WORKDIR /app
 COPY . /app
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-EXPOSE 8000
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python3", "manage.py", "runserver"]
--- dj_diplom/settings.py
+++ dj_diplom/settings.py
@@ -100,24 +100,24 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
-        'NAME': os.environ.get("SQL_DATABASE", "dj_diplom"),
-        'USER': os.environ.get("SQL_USER", "owner_dj_diplom"),
-        'PASSWORD': os.environ.get("SQL_PASSWORD", "owner_dj_diplom"),
-        'HOST': os.environ.get("SQL_HOST", "localhost"),
-        'PORT': os.environ.get("SQL_PORT", "5432"),
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'mydatabase',
+#         'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+#         'NAME': os.environ.get("SQL_DATABASE", "dj_diplom"),
+#         'USER': os.environ.get("SQL_USER", "owner_dj_diplom"),
+#         'PASSWORD': os.environ.get("SQL_PASSWORD", "owner_dj_diplom"),
+#         'HOST': os.environ.get("SQL_HOST", "localhost"),
+#         'PORT': os.environ.get("SQL_PORT", "5432"),
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
