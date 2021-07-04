666fe85b5bf84ce8b68483ea8d0374409dd8f7ff
Ozodchik <dilovardehkanov730@gmail.com>
2021-07-04 22:57:41 +0300

    changed settings.py

--- dj_diplom/settings.py
+++ dj_diplom/settings.py
@@ -102,32 +102,32 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#     }
+# }
 
-# if os.getenv("TESTING") == "1":
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': 'mydatabase',
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': os.getenv("SQL_ENGINE"),
-#             'NAME': os.getenv("SQL_DATABASE"),
-#             'USER': os.getenv("SQL_USER"),
-#             'PASSWORD': os.getenv("SQL_PASSWORD"),
-#             'HOST': os.getenv("SQL_HOST", "localhost"),
-#             'PORT': os.getenv("SQL_PORT", "5432"),
-#         }
-#     }
-#
+if os.getenv("TESTING") == "1":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'mydatabase',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv("SQL_ENGINE", "django.db.backends.postgresql"),
+            'NAME': os.getenv("SQL_DATABASE", "dj_diplom"),
+            'USER': os.getenv("SQL_USER", "owner_dj_diplom"),
+            'PASSWORD': os.getenv("SQL_PASSWORD", "owner_dj_diplom"),
+            'HOST': os.getenv("SQL_HOST", "dj_diplom_db_1"),
+            'PORT': os.getenv("SQL_PORT", "5432"),
+        }
+    }
+
 # DATABASES['default'] = dj_database_url.config()
 
 # Password validation
