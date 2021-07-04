0f5808705c2c97c5487f1f876688bc33fe657dab
Ozodchik <dilovardehkanov730@gmail.com>
2021-07-04 21:33:13 +0300

    changed reqs.txt and added Procfile

--- dj_diplom/settings.py
+++ dj_diplom/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import django_heroku
 load_dotenv()
-
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
